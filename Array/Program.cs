a611ec8ce817c761c0b5ccf6d827cf21a2f9287d
erfanghasemi1 <erfanerfanghasemi1@gmail.com>
2025-08-23 04:14:24 +0330

    array attributes

---
+++ Array/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Program
+{
+    public class EnteryPoint
+    {
+        private static ArrayDataStructure _array;
+
+        static EnteryPoint()
+        {
+            _array = new ArrayDataStructure();
+        }
+
+        public static void Main()
+        {
+            _array.Attributes();
+        }
+    }
+
+    public class ArrayDataStructure
+    {
+        private int[] numbers;
+
+        public ArrayDataStructure()
+        {
+            numbers = new int[] { 10, -5, 9, 100, 59, 72, 3, 1000, 1, 1 };
+        }
+
+        /// <summary>
+        /// prints the main attributes of Array
+        /// </summary>
+        public void Attributes()
+        {
+            int length = numbers.Length;
+            Console.WriteLine($"Length is : {length}");
+
+            int rank = numbers.Rank;
+            Console.WriteLine($"Rank is : {rank}");
+
+            bool IsFixedSize = numbers.IsFixedSize;
+            Console.WriteLine($"Is Fixed Size : {IsFixedSize}");
+
+            // You can use at as locker ( in Array there is no difference between Array and ICollection)
+            object SyncRoot = numbers.SyncRoot;
+        }
+    }
+}
